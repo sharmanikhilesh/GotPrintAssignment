1c4822b15d15240bbdd4d2832d97ff51c8a1cc20
sharmanikhilesh <nikhilesh.sharma45@gmail.com>
2016-01-18 10:27:28 +0530

    Revert "Db update"
    This reverts commit 99f2a8639f5d61e197c6e8ef83d510c5102a789b.

--- usernotes/src/main/java/org/gotprint/assignment/usernotes/dbservice/NotesService.java
+++ usernotes/src/main/java/org/gotprint/assignment/usernotes/dbservice/NotesService.java
@@ -1,23 +1,12 @@
 package org.gotprint.assignment.usernotes.dbservice;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
-import org.gotprint.assignment.usernotes.entity.NoteEntity;
-import org.gotprint.assignment.usernotes.entity.UserEntity;
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class NotesService {
 	
 	private static SessionFactory sessionFactory = null;
     
-    private final String NotesByUser = "NoteEntity.byUserEmail";
-    private final String RemoveNotesByUser = "NoteEntity.delete.byUserEmail";
-    private final String EmailQueryVar = "email";
     
     public NotesService(){
     	setSessionFactory();
@@ -26,64 +15,5 @@ public class NotesService {
     private void setSessionFactory() {
         sessionFactory = HibernateUtils.getSessionFactory();
     }
-    
-	public List<NoteEntity> getAllNotesByUser(String email){
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		Query query = session.getNamedQuery(NotesByUser);
-		query.setString(EmailQueryVar, email);
-		List<NoteEntity> notes = (List<NoteEntity>)query.list();
-		session.close();
-		return notes;
-	}
-	
-	public void addNoteForUser(String email, NoteEntity note){
-		UserEntity user = new UserEntity();
-		user.setEmail(email);
-		note.setUser(user);
-		
-		note.setCreateTime(new Date());
-		note.setLastUpdateTime(new Date());
-		
-		
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.save(note);
-		session.getTransaction().commit();
-		session.close();
-
-	}
-	
-	public void removeAllNotes(String email){
-		
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		Query query = session.getNamedQuery(RemoveNotesByUser);
-		query.setString(EmailQueryVar, email);
-		query.executeUpdate();
-		session.close();
-	
-	}
-	
-	public void removeNote(int noteID){
-		// TODO : Verify for messageID
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		NoteEntity note = (NoteEntity) session.get(NoteEntity.class, noteID);
-		session.delete(note);
-		session.getTransaction().commit();
-		session.close();
-	}
-	
-	public void updateNote(int noteID, NoteEntity newNote){
-		//TODO : Verify for messageID
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		NoteEntity note = (NoteEntity) session.get(NoteEntity.class, noteID);
-		note.setNote(newNote.getNote());
-		session.update(note);
-		session.getTransaction().commit();
-		session.close();
-	}
 
 }
